29b9bb229f32fe100193aa2f0560de59b34585d4
Baker Satou <bsatou@su-i4.21-school.ru>
2019-09-06 19:59:39 +0300

    fix memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: bsatou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 19:19:24 by bsatou            #+#    #+#             */
-/*   Updated: 2019/09/03 19:53:17 by bsatou           ###   ########.fr       */
+/*   Updated: 2019/09/06 19:58:34 by bsatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,16 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i;
+	size_t			i;
+	unsigned char	*str1;
+	unsigned char	*str2;
 
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
 	i = 0;
-	while (i < n)
-	{
-		if (((unsigned char *)s1)[i] == ((unsigned char *)s2)[i])
-			i++;
-		else
-			break ;
-	}
-	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+	while (i < n && str1[i] == str2[i])
+		i++;
+	if (i == n)
+		return (str1[n - 1] - str2[n - 1]);
+	return (str1[i] - str2[i]);
 }
