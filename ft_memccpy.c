d04b75f13d1645726765624f7c8d03d00758f95e
Baker Satou <bsatou@su-i4.21-school.ru>
2019-09-06 19:19:50 +0300

    fix memccpy

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: bsatou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 18:43:14 by bsatou            #+#    #+#             */
-/*   Updated: 2019/09/06 18:43:27 by bsatou           ###   ########.fr       */
+/*   Updated: 2019/09/06 19:18:58 by bsatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,12 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	dest2 = (unsigned char *)dest;
 	src2 = (unsigned char *)src;
 	i = 0;
-	while (i < n && src2[i] != c)
+	while (i < n && src2[i] != (unsigned char)c)
 	{
 		dest2[i] = src2[i];
 		i++;
 	}
-	if (src2[i] == c)
+	if (src2[i] == (unsigned char)c)
 	{
 		dest2[i] = src2[i];
 		return (dest2 + i + 1);
