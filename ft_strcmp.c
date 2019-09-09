c49b147dc5724370ba1968fc276471b51556eb8d
Baker Satou <bsatou@su-j2.21-school.ru>
2019-09-09 17:47:13 +0300

    fix strstr & strnstr

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: bsatou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 12:04:10 by bsatou            #+#    #+#             */
-/*   Updated: 2019/07/08 13:46:48 by bsatou           ###   ########.fr       */
+/*   Updated: 2019/09/09 17:41:50 by bsatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@ int		ft_strcmp(const char *s1, const char *s2)
 	int i;
 
 	i = 0;
-	while ((s1[i] != '\0') && (s2[i] != '\0') && (s1[i] == s2[i]))
+	while ((s1[i] != '\0') && (s2[i] != '\0') && \
+	((unsigned char)s1[i] == (unsigned char)s2[i]))
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: bsatou <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/08 12:04:10 by bsatou            #+#    #+#             */
-/*   Updated: 2019/09/03 18:34:58 by bsatou           ###   ########.fr       */
+/*   Updated: 2019/09/09 17:46:09 by bsatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	while ((s1[i] != '\0') && (s2[i] != '\0') && (s1[i] == s2[i]) && (i < n))
+	while ((s1[i] != '\0') && (s2[i] != '\0') && \
+	((unsigned char)s1[i] == (unsigned char)s2[i]) && (i < n))
 		i++;
-	return (s1[i] - s2[i]);
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
