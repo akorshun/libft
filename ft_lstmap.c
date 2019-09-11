15025219865e094c6e579af8cdbeb8ca982b6416
Baker Satou <bsatou@at-d3.21-school.ru>
2019-09-11 14:01:58 +0300

    do lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: bsatou <bsatou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/10 20:52:02 by bsatou            #+#    #+#             */
-/*   Updated: 2019/09/10 20:56:18 by bsatou           ###   ########.fr       */
+/*   Updated: 2019/09/11 14:00:49 by bsatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,29 @@
 t_list	*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem))
 {
 	t_list *new_list;
+	t_list *buf;
+	t_list *start;
 
+	if (!lst || !f)
+		return (NULL);
 	new_list = (t_list*)malloc(sizeof(t_list));
 	if (!new_list)
 		return (NULL);
+	buf = f(lst);
+	new_list = ft_lstnew(buf->content, buf->content_size);
+	if (!new_list)
+		return (NULL);
+	start = new_list;
+	lst = lst->next;
 	while (lst)
 	{
-		new_list = f(lst);
-		lst->next;
-		
+		buf = f(lst);
+		if (ft_lstnew(buf->content, buf->content_size))
+		{
+			new_list = ft_lstnew(buf->content, buf->content_size);
+			new_list = new_list->next;
+		}
+		lst = lst->next;
 	}
+	return (start);
 }
