c6a995e4e5ead7aaeceeeaf98ba605da598f01ca
Baker Satou <bsatou@su-i2.21-school.ru>
2019-09-12 18:58:06 +0300

    fix strsplit

--- ft_count_letters.c
+++ ft_count_letters.c
@@ -6,7 +6,7 @@
 /*   By: bsatou <bsatou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 14:41:16 by bsatou            #+#    #+#             */
-/*   Updated: 2019/09/11 15:14:03 by bsatou           ###   ########.fr       */
+/*   Updated: 2019/09/12 18:57:44 by bsatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,10 @@ size_t	ft_count_letters(const char *str, char c)
 			i++;
 		while (str[i] != c && str[i])
 		{
-			i++;
-			letters++;
 			if (str[i] != c && str[i + 1] == c && str[i + 1])
 				return (letters + 1);
+			i++;
+			letters++;
 		}
 	}
 	return (letters);
