c2026f8da195f6e4b8b609c73031582ab2d2c18b
Baker Satou <bsatou@su-i2.21-school.ru>
2019-09-12 17:14:41 +0300

    fix ft_strnequ

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnequ.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsatou <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: bsatou <bsatou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/03 20:27:35 by bsatou            #+#    #+#             */
-/*   Updated: 2019/09/03 20:32:34 by bsatou           ###   ########.fr       */
+/*   Updated: 2019/09/12 17:14:19 by bsatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int		ft_strnequ(char const *s1, char const *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	if (!s1 || !s2)
+	if (!s1 || !s2 || ft_strnlen(s1, n) != ft_strnlen(s2, n))
 		return (0);
 	while (s1[i] && s2[i] && i < n)
 	{
