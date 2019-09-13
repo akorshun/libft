d52f9e051488eecba2e716e4be4f16f97b5c0755
Baker Satou <bsatou@vo-i5.21-school.ru>
2019-09-13 16:15:08 +0300

    fix free strsplit

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: bsatou <bsatou@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 14:06:53 by bsatou            #+#    #+#             */
-/*   Updated: 2019/09/12 19:52:48 by bsatou           ###   ########.fr       */
+/*   Updated: 2019/09/13 16:08:08 by bsatou           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,8 @@ static size_t	ft_len_word(char const *s, size_t k, char c)
 	return (len);
 }
 
-static void		ft_strsplit_clear(char **tab)
+static void		ft_strsplit_clear(char **tab, size_t i)
 {
-	size_t i;
-
-	i = 0;
 	while ((int)i >= 0)
 	{
 		free(tab[i]);
@@ -48,10 +45,9 @@ char			**ft_strsplit(char const *s, char c)
 	{
 		while (s[k] == c && s[k])
 			k++;
-		if (!(tab[i] = (char*)malloc(sizeof(char) * \
-		(ft_len_word(s, k, c) + 1))))
+		if (!(tab[i] = ft_strnew((ft_len_word(s, k, c)))))
 		{
-			ft_strsplit_clear(tab);
+			ft_strsplit_clear(tab, i);
 			return (NULL);
 		}
 		tab[i] = ft_strsub(s, k, ft_len_word(s, k, c));
